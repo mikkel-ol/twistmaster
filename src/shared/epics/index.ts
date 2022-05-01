a854c9297dc2baa7f483e0dafebae6cb1c0d05f3
Mikkel O. Laursen <m@ikkel.dk>
2022-05-01 22:00:53 +0200

    Add redux-observable

---
+++ src/shared/epics/index.ts
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import { combineEpics } from 'redux-observable';
+
+export const rootEpic = combineEpics();
+
+export const rootReducer = combineReducers({});
--- src/shared/store/store.ts
+++ src/shared/store/store.ts
@@ -1,11 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
-import attackSpeed from "../states/attack-speed";
+import { configureStore } from '@reduxjs/toolkit';
 
-export const store = configureStore({
-  reducer: {
-    attackSpeed,
-  },
-});
+import { rootEpic } from '../epics';
+import { epicMiddleware } from '../middlewares';
+import attackSpeed from '../states/attack-speed';
+
+const setup = () => {
+  const store = configureStore({
+    reducer: {
+      attackSpeed,
+    },
+    middleware: [epicMiddleware],
+  });
+
+  epicMiddleware.run(rootEpic);
+
+  return store;
+};
+
+export const store = setup();
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
