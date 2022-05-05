4323fac1d8ef0ee8faa082d6ac798dbc1969f51a
Mikkel O. Laursen <m@ikkel.dk>
2022-05-05 21:29:36 +0200

    Remove input field

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import { useShortcut } from "src/shared/hooks/shortcutListenerHook";
 import { Shortcut } from "src/shared/models/shortcut";
-import { setAttackSpeed } from "src/shared/states/attack-speed";
 import { toggle } from "src/shared/states/attacking";
 import { useAppDispatch } from "src/shared/store";
 import "./App.scss";
@@ -16,10 +15,6 @@ export const App = () => {
     <div className="App">
       <InfoTooltip></InfoTooltip>
       <SwingTimer></SwingTimer>
-      <input
-        type="number"
-        onChange={(ev) => dispatch(setAttackSpeed(Number(ev.target.value)))}
-      />
     </div>
   );
 };
